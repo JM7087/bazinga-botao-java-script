9c5e0677da7513c03dc424c3614cdb613f42f833
João Marcos de Oliveira santos <joaomarcos@jm7087.com>
2024-08-01 10:48:17 -0300

    mudar cor de fundo

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 var audio = new Audio('som/bazinga.mp3');
+var currentColorIndex = 0;
+
+// Lista de cores
+var colors = ['#1212de', '#c200cc', '#fff700', '#4682b4', '#32cd32', '#ffa500', '#9fa19f', '#ed2222']; 
 
 function playSound() {
   var clone = audio.cloneNode();
   clone.currentTime = audio.currentTime;
   clone.play();
+  
+  mudarCorDefundo();
+}
+
+function mudarCorDefundo() {
+  // Mudar a cor de fundo
+  document.body.style.backgroundColor = colors[currentColorIndex];
+  currentColorIndex = (currentColorIndex + 1) % colors.length; // Avança para a próxima cor
 }
